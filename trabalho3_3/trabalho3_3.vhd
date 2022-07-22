f31d86de2c51a783b5cc286b78f5775788d9699f
Jonas de Oliveira <jonas.filho.ext@synchro.com.br>
2022-07-21 23:38:46 -0300

    main - implementação total do trabalho3_3

--- trabalho3_3/trabalho3_3.vhd
+++ trabalho3_3/trabalho3_3.vhd
@@ -26,23 +26,16 @@ ARCHITECTURE hardware OF trabalho3_3 is
 		  );
     END COMPONENT;
 
-    SIGNAL a, b   : std_logic_vector(15 downto 0);
-    SIGNAL f1, f2 : std_logic := '0';
+    SIGNAL a, b, counter    : std_logic_vector(15 downto 0) := "0000000000000000";
+    SIGNAL f1, f2, isRunnig : std_logic                     := '0';
 
-    SIGNAL o      : std_logic_vector(15 downto 0) := "0000000000000000";
-    SIGNAL counter: std_logic_vector(15 downto 0) := "0000000000000000";
-
-    SIGNAL isRunnig: std_logic := '0';
-    SIGNAL tempisRunnig: std_logic := '0';
 
 BEGIN
-    -- clk <= not clk after ClockPeriod / 2;
-PROCESS (e1, e2, i, a, b, f1, f2, o, counter, clk, isRunnig)
+PROCESS (e1, e2, i, a, b, f1, f2, counter, clk, isRunnig)
 BEGIN
     -- Clock na borda de subida
     IF rising_edge(clk) THEN
-        -- Para mostrar valores em tempo real
-        o <= i;
+        -- Pode-se criar um signal e displays para mostrar o i em tempo real
 
         -- Recebe os valores de A
         IF (e1 = '0' and isRunnig = '0') THEN
@@ -64,40 +57,36 @@ BEGIN
             l2  <= '0';
         END IF;
 
-        -- Calcula o quociente
+        -- Começa a rodar o programa
         IF (f1  = '1' and f2  = '1' and isRunnig = '0') THEN
+            counter <= "0000000000000000";
+            isRunnig <= '1';
+            f1  <= '0';
+            f2  <= '0';
+        END IF;
+
+        -- Calcula o quociente
+        IF (isRunnig = '1') THEN
             IF (a >= b) THEN
                 a       <= a - b;
                 counter <= counter + '1';
             ELSE
-                tempisRunnig <= '1';
-            END IF;
-
-            -- Finalizou a divisão, então flags são zeradas 
-            IF (tempisRunnig = '1') THEN
-                f1  <= '0';
-                f2  <= '0';
-
                 -- Se A < B, então da erro
                 IF (counter = "0000000000000000") THEN
-                    o   <= "1110111011101110";
-                ELSE
-                    o <= counter;
+                    counter   <= "1110111011101110";
                 END IF;
 
-                -- Zera o contador
-                counter <= "0000000000000000";
-                isRunnig <= '1';
+                isRunnig <= '0';
             END IF;
         END IF;
     END IF;
 
 END PROCESS;
 
-ent0: display_sete_seg PORT MAP (o(15 downto 12), d1);
-ent1: display_sete_seg PORT MAP (o(11 downto 8), d2);
-ent2: display_sete_seg PORT MAP (o(7 downto 4), d3);
-ent3: display_sete_seg PORT MAP (o(3 downto 0), d4);
+ent0: display_sete_seg PORT MAP (counter(15 downto 12), d1);
+ent1: display_sete_seg PORT MAP (counter(11 downto 8), d2);
+ent2: display_sete_seg PORT MAP (counter(7 downto 4), d3);
+ent3: display_sete_seg PORT MAP (counter(3 downto 0), d4);
 
 f1out <= f1;
 f2out <= f2;
