d80d512eaa770d1bbe06eb1228a7f4fd29381a80
Jonas de Oliveira <jonas.filho.ext@synchro.com.br>
2022-07-21 22:56:15 -0300

    main - implementação parcial do trabalho3_3 2

--- trabalho3_3/trabalho3_3.vhd
+++ trabalho3_3/trabalho3_3.vhd
@@ -6,9 +6,14 @@ use ieee.numeric_std.all;
 ENTITY trabalho3_3 IS
 	PORT ( 
             e1, e2          : IN std_logic;
+            clk             : IN std_logic;
             i               : IN std_logic_vector(15 downto 0);
             d1, d2, d3, d4  : OUT std_logic_vector(6 downto 0);
-            l1, l2          : OUT std_logic
+            l1, l2          : OUT std_logic;
+            aout, bout      : OUT std_logic_vector(15 downto 0);
+            f1out, f2out    : OUT std_logic;
+            counterOut      : OUT std_logic_vector(15 downto 0);
+            isRunnigOut     : OUT std_logic
 		  );
 END;
 
@@ -21,68 +26,70 @@ ARCHITECTURE hardware OF trabalho3_3 is
 		  );
     END COMPONENT;
 
-    CONSTANT ClockFrequency : integer := 1e3;
-    CONSTANT ClockPeriod    : time    := 100 ms / ClockFrequency;
-
     SIGNAL a, b   : std_logic_vector(15 downto 0);
-    SIGNAL f1, f2 : std_logic;
+    SIGNAL f1, f2 : std_logic := '0';
 
-    SIGNAL o      : std_logic_vector(15 downto 0);
-    SIGNAL counter: std_logic_vector(15 downto 0);
+    SIGNAL o      : std_logic_vector(15 downto 0) := "0000000000000000";
+    SIGNAL counter: std_logic_vector(15 downto 0) := "0000000000000000";
 
-    SIGNAL clk : std_logic := '1';
+    SIGNAL isRunnig: std_logic := '0';
+    SIGNAL tempisRunnig: std_logic := '0';
 
 BEGIN
-    clk <= not clk after ClockPeriod / 2;
-PROCESS (e1, e2, i, a, b, f1, f2, o, counter, clk)
+    -- clk <= not clk after ClockPeriod / 2;
+PROCESS (e1, e2, i, a, b, f1, f2, o, counter, clk, isRunnig)
 BEGIN
-    -- Para mostrar valores em tempo real
-    o <= i;
+    -- Clock na borda de subida
+    IF rising_edge(clk) THEN
+        -- Para mostrar valores em tempo real
+        o <= i;
 
-    -- Recebe os valores de A
-    IF (e1 = '0') THEN
-        a   <= i;
-        f1  <= '1';
-        l1  <= '1';
-    ELSE
-        l1  <= '0';
-    END IF;
-
-    -- Recebe os valores de B
-    IF (e2 = '0') THEN
-        b   <= i;
-        f2  <= '1';
-        l2  <= '1';
-    ELSE
-        l2  <= '0';
-    END IF;
-
-    -- Calcula o quociente
-    IF (f1  = '1' and f2  = '1') THEN
-        counter <= "0000000000000000";
-
-        -- Loop para encontrar o quociente
-        -- IF (rising_edge(clk) and a >= b) THEN
-        IF (a >= b) THEN
-        IF rising_edge(clk) THEN
-            a       <= a - b;
-            counter <= counter + '1';
-            ELSE 
-            counter <= counter + '0';
-            END IF;
-            END IF;
-
-
-        -- Se A < B, então da erro
-        IF (counter = "0000000000000000") THEN
-            o <= "1110111011101110";
-        -- Se counter = 9999 então da erro por limitação do quartus.
-        ELSIF (counter = "0010011100001111") THEN
-            o <= "1111111111111111";
+        -- Recebe os valores de A
+        IF (e1 = '0' and isRunnig = '0') THEN
+            a   <= i;
+            f1  <= '1';
+            l1  <= '1';
+            -- add code: wait 50 ms
         ELSE
-            o <= counter;
+            l1  <= '0';
         END IF;
 
+        -- Recebe os valores de B
+        IF (e2 = '0' and isRunnig = '0') THEN
+            b   <= i;
+            f2  <= '1';
+            l2  <= '1';
+            -- add code: wait 50 ms
+        ELSE
+            l2  <= '0';
+        END IF;
+
+        -- Calcula o quociente
+        IF (f1  = '1' and f2  = '1' and isRunnig = '0') THEN
+            IF (a >= b) THEN
+                a       <= a - b;
+                counter <= counter + '1';
+            ELSE
+                tempisRunnig <= '1';
+            END IF;
+
+            -- Finalizou a divisão, então flags são zeradas 
+            IF (tempisRunnig = '1') THEN
+                f1  <= '0';
+                f2  <= '0';
+
+                -- Se A < B, então da erro
+                IF (counter = "0000000000000000") THEN
+                    o   <= "1110111011101110";
+                ELSE
+                    o <= counter;
+                END IF;
+
+                -- Zera o contador
+                counter <= "0000000000000000";
+                isRunnig <= '1';
+            END IF;
+        END IF;
     END IF;
 
 END PROCESS;
@@ -91,5 +98,12 @@ ent0: display_sete_seg PORT MAP (o(15 downto 12), d1);
 ent1: display_sete_seg PORT MAP (o(11 downto 8), d2);
 ent2: display_sete_seg PORT MAP (o(7 downto 4), d3);
 ent3: display_sete_seg PORT MAP (o(3 downto 0), d4);
+
+f1out <= f1;
+f2out <= f2;
+isRunnigOut <= isRunnig;
+aout <= a;
+bout <= b;
+counterOut <= counter;
 
 END hardware;
